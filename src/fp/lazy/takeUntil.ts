8dbbaf7545b13d3a2957d7875724b1d960d87129
roseline124 <guseod24@gmail.com>
2021-08-16 17:42:39 +0900

    fix: takeUntil

--- src/fp/lazy/takeUntil.ts
+++ src/fp/lazy/takeUntil.ts
@@ -1,10 +1,28 @@
 import curry from '../utils/curry';
 
-const takeUntil = curry(function* (fn: any, iter?: any) {
-  for (const item of iter) {
-    if (fn(item)) return;
-    yield item;
-  }
+const nop = Symbol.for('nop');
+const toIter = (iter?: any) => iter?.[Symbol.iterator]?.();
+
+const takeUntil = curry(function (fn: any, iter?: any) {
+  iter = toIter(iter);
+  if (!iter) throw new Error('It is not iterable');
+
+  return (function recur(): any {
+    const res: any[] = [];
+    let cur = iter.next();
+    while (!cur.done) {
+      const result = (res.push(cur.value), fn(cur.value));
+      if (result instanceof Promise) {
+        return result
+          .then((v) => (v ? result : recur()))
+          .catch((e) => (e == nop ? recur() : Promise.reject(e)));
+      }
+
+      if (result) break;
+    }
+
+    return res;
+  })();
 });
 
 export default takeUntil;
